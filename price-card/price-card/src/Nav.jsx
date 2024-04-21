d7643a28394c597a0cb4bc47ace5587dc3c6270a
jaganarasu <jaganarasu.aj@gmail.com>
2024-04-22 03:27:28 +0530

    price card added

---
+++ price-card/price-card/src/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Nav() {
+    const updateMenu = () => {};
+  
+    return (
+      <nav className="menu">
+        <p>Price Card Using React js</p>
+        </nav>
+        );
+}
+
+export default Nav;
